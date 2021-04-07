d0944f5004ab7dcf877e6194a0a39301649c6a94
Aliaksandr Melnik <melnik.alexander.mail@gmail.com>
2021-04-07 23:46:14 +0300

    added task with systemProperties to build gradle

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        String selenoidUrl = System.getProperty("selenoid_url");
+        String selenoidUrl = System.getProperty("selenoid.url");
         Configuration.remote = "https://user1:1234@" + selenoidUrl + "/wd/hub/";
     }
 
